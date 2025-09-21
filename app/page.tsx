10c0b66a68eaae1df9b1bdd3dd1c4c78aefc9147
Karthik Saiharsh <ks.the.visionary@gmail.com>
2025-09-21 21:03:09 +0530

    Added Logo in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
 import AuroraBackgroundHome from "@/components/home/AuroraBackgroundHome";
 import { Button } from "@/components/ui/button";
 import Link from "next/link"; 
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <AuroraBackgroundHome>
-        <div className="relative select-none z-10 flex min-h-screen flex-col items-center justify-center px-6 text-center">
+        <div className="relative select-none z-10 flex min-h-screen flex-col items-center justify-center px-6 text-center mb-20">
+          <Image
+            src="/logo.png"
+            alt="Gen Ojas Logo"
+            width={300}
+            height={300}
+            className="-mb-20"
+          />
           <h1 className="text-4xl font-black font-jakarta text-blue-950 sm:text-5xl md:text-6xl">
             Welcome to{" "}
             <span className="bg-gradient-to-r font-jakarta from-blue-700 to-blue-400 bg-clip-text text-transparent [text-shadow:0_0_10px_#93c5fd]">
-              Gen Ojas
+              GenOjas
             </span>
           </h1>
           <p className="text-blue-900 mt-4 text-lg font-inter dark:text-gray-300 sm:text-xl md:text-2xl">
